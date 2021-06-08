0806607d9ad0c73b59f4d6053f4c0fff7aceb4ce
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-06-08 22:24:37 +0900

    Add: Nav buttons at bottom work now

--- src/pages/Main/Editor/BottomButtons/index.tsx
+++ src/pages/Main/Editor/BottomButtons/index.tsx
@@ -3,28 +3,46 @@ import { useDispatch, useSelector } from "react-redux";
 import Button from "../../../../components/Button";
 
 import { State } from "../../../../config/types";
+import { setActiveWordAction } from "../../../../store/actions/actions";
 
 import style from "./BottomButtons.module.scss";
 
-function BottomButtons({dismissEditing}: {dismissEditing: ()=>void;}) {
-
+function BottomButtons({ dismissEditing }: { dismissEditing: () => void }) {
   const dispatch = useDispatch();
   const activeWord = useSelector((state: State) => state.activeWord);
   const flashContent = useSelector((state: State) => state.flashContent);
 
   const handleBack = () => {
+    if (activeWord && flashContent) {
+      const currentIndex = flashContent?.indexOf(activeWord);
+      if (currentIndex && currentIndex > 0) {
+        dispatch(setActiveWordAction(flashContent[currentIndex - 1]));
+      } else {
+        dispatch(setActiveWordAction(flashContent[flashContent.length - 1])); // Cycle round
+      }
+    }
+  };
 
-    // currentIndex = 
-
-  }
+  const handleNext = () => {
+    if (activeWord && flashContent) {
+      const currentIndex = flashContent?.indexOf(activeWord);
+      console.log(currentIndex, flashContent.length);
+      if (currentIndex + 1 && currentIndex + 1 < flashContent.length) {
+        //current+1 because 0 is false, of course...
+        dispatch(setActiveWordAction(flashContent[currentIndex + 1]));
+      } else {
+        dispatch(setActiveWordAction(flashContent[0])); // Cycle round
+      }
+    }
+  };
 
   return (
     <div className={style.bottomButtons}>
-      <Button>
+      <Button onClick={handleBack}>
         <i className="fas fa-angle-left"></i>
       </Button>
       <Button>Make Cards</Button>
-      <Button>
+      <Button onClick={handleNext}>
         <i className="fas fa-angle-right"></i>
       </Button>
     </div>
