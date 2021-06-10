ef54f6a17c8a4a298fa4d03f802be299a8efc3ab
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-06-10 22:16:00 +0900

    Add: loader

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,95 @@
+import style from "./About.module.scss";
+
+function About() {
+  return (
+    <div className={style.main}>
+      <div className={style.title}>About</div>
+      <details className={style.detailBox}>
+        <summary>What does it do?</summary>
+        <p>Flashmachine is a wordlist app for Korean and English learners.</p>
+        <p>It produces Anki-compatible flashcard content.</p>
+        <p>
+          Fill in words as you are reading, and when you are done, submit the
+          words to see the definitions.
+        </p>
+        <p>
+          You may then edit the resulting definitions into memorable flashcard
+          content. Press 'Make cards' to receive a .txt file you can import into
+          your Anki deck!
+        </p>
+      </details>
+      <details className={style.detailBox}>
+        <summary>What is Anki?</summary>
+        <p>
+          <a href="https://apps.ankiweb.net/" target="_blank" rel="noreferrer">
+            Anki
+          </a>{" "}
+          is a spaced-repetition learning tool - a flashcard app - that is great
+          for learning languages. You can use it on PC or Android for free, and
+          iOS for a price. I've been using it daily since 2014.
+        </p>
+      </details>
+      <details className={style.detailBox}>
+        <summary>Why only 30 words?</summary>
+        <p>
+          The words are sourced from a third-party and in order not to swamp
+          their servers with requests, I limited the word list to 30.
+        </p>
+      </details>
+      <details className={style.detailBox}>
+        <summary>Only Korean/English?</summary>
+        <p>
+          The dictionary I'm using doesn't really support other languages that
+          well. Plus using another dictionary would mean basically building a
+          whole 'nother app. It's not on the cards right now.
+        </p>
+      </details>
+      <details className={style.detailBox}>
+        <summary>How do I import the cards?</summary>
+        <p>
+          Importing from .txt is possible on a computer version of the app, not
+          a phone version.
+        </p>
+        <p>
+          Open the app on your computer, select the deck you want to import to,
+          and choose File --&gt; import.
+        </p>
+        <p>
+          In the next dialog box, make sure the option 'text separated by tabs
+          or semicolons' is chosen.
+        </p>
+        <p>
+          You'll see another dialog box that will help you to get the right
+          fields from your flashmachine file into your anki deck.
+        </p>
+        <p>
+          That's the most complicated part, take your time because it's awkward
+          to undo. Go for it!
+        </p>
+      </details>
+      <details className={style.detailBox}>
+        <summary>Tips for using Flashmachine</summary>
+        <p>If you're on the computer, you can add words using the enter key.</p>
+        <p>
+          You can delete the last word with <kbd>ctrl</kbd> +{" "}
+          <kbd>backspace</kbd>.
+        </p>
+        <p>
+          You can also submit your word list with <kbd>ctrl</kbd> +{" "}
+          <kbd>enter</kbd>!
+        </p>
+      </details>
+      <details className={style.detailBox}>
+        <summary>About the dev</summary>
+        <p>I'm Tobias. I'm from the UK, but I live in Korea.</p>
+        <p>I used to be an English teacher here but now I'm a web developer</p>
+        <p>
+          Anki helped me out a lot learning Korean, so this is something to help
+          Anki out (and all you other Korean or English learners!)
+        </p>
+      </details>
+    </div>
+  );
+}
+
+export default About;
