67c52c16744b1e595adcb35dcd7e76b7f6b48492
BruceJi7 <revolverc24@gmail.com>
2022-03-17 19:36:50 +0900

    Add word counter to word entry box

--- src/pages/Main/WordEntryBox/WordEntryBox.tsx
+++ src/pages/Main/WordEntryBox/WordEntryBox.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import { WORD_LIMIT } from "../../../const";
+
 import styles from "./WordEntryBox.module.scss";
 
 function WordEntryBox() {
   return (
     <div className={styles.WordEntryBox}>
       <input className={styles.Input} placeholder="Add words here!" />
+      <div className={styles.Counter}>{`0 / ${WORD_LIMIT}`}</div>
     </div>
   );
 }
